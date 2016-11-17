b8c25881aaae32b2fdb60f333cb98e3d94799fb8
Funky Noodles <nickasds@hotmail.com>
2016-11-17 11:43:05 -0600

    Tested rapid json

--- realtimestock/StockQuote.cpp
+++ realtimestock/StockQuote.cpp
@@ -1,6 +1,11 @@
 #include "StockQuote.h"
 #include <iostream>
+
 #include <curl\curl.h>
+#include <rapidjson\document.h>
+
+using namespace rapidjson;
+using namespace std;
 
 
 StockQuote::StockQuote()
@@ -9,13 +14,28 @@ StockQuote::StockQuote()
 	if (curl) {
 		CURLcode res;
 		curl_easy_setopt(curl, CURLOPT_URL, "http://finance.google.com/finance/info?q=NVDA");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
 		res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
-		std::cout << res << std::endl;
+		readBuffer = readBuffer.substr(6, readBuffer.size() - 9);
+		std::cout << readBuffer << std::endl;
+		rapidjson::Document document;
+		document.Parse(readBuffer.c_str());
+		assert(document.IsObject());
+		assert(document.HasMember("t"));
+		assert(document["t"].IsString());
+		std::cout << document["t"].GetString() << std::endl;
 	}
 }
 
 
 StockQuote::~StockQuote()
 {
+}
+
+size_t StockQuote::curlWriteCallback(void * contents, size_t size, size_t nmemb, void * userp)
+{
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
 }
--- realtimestock/StockQuote.h
+++ realtimestock/StockQuote.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <string>
+
 class StockQuote
 {
 public:
 	StockQuote();
 	~StockQuote();
+private:
+	std::string readBuffer;
+	static size_t curlWriteCallback(void * contents, size_t size, size_t nmemb, void * userp);
 };
 
